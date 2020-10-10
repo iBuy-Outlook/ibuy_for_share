6c510f5a55ca501172c77a66857dad302c811b31
iBuy-Outlook <49996989+iBuy-Outlook@users.noreply.github.com>
2020-10-10 19:21:42 -0400

    Second commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'home_widget.dart';
+import 'package:ibuy_mac_1/views/first_view.dart';
+import 'package:ibuy_mac_1/views/sign_up_view.dart';
+import 'package:ibuy_mac_1/widgets/provider_widget.dart';
+import 'package:ibuy_mac_1/services/auth_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      auth: AuthService(),
+      child: MaterialApp(
+        title: "Travel Budget App",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeController(),
+        routes: <String, WidgetBuilder>{
+          '/signUp': (BuildContext context) => SignUpView(authFormType: AuthFormType.signUp),
+          '/signIn': (BuildContext context) => SignUpView(authFormType: AuthFormType.signIn),
+          '/anonymousSignIn': (BuildContext context) => SignUpView(authFormType: AuthFormType.anonymous),
+          '/home': (BuildContext context) => HomeController(),
+        },
+      ),
+    );
+  }
+}
+
+class HomeController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final AuthService auth = Provider.of(context).auth;
+    return StreamBuilder<String>(
+      stream: auth.onAuthStateChanged,
+      builder: (context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final bool signedIn = snapshot.hasData;
+          return signedIn ? Home() : FirstView();
+        }
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
